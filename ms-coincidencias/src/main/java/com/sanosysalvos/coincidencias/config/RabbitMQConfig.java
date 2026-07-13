46cae60fce34855f5cf13826b7599317bfac0cb5
clmoralesl <cl.moralesl@duocuc.cl>
2026-07-13 03:24:48 -0400

    Corrige tipo de exchange en ms-coincidencias de TopicExchange a DirectExchange para consistencia con el resto del sistema

--- ms-coincidencias/src/main/java/com/sanosysalvos/coincidencias/config/RabbitMQConfig.java
+++ ms-coincidencias/src/main/java/com/sanosysalvos/coincidencias/config/RabbitMQConfig.java
@@ -1,6 +1,6 @@
 package com.sanosysalvos.coincidencias.config;
 
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -15,8 +15,8 @@ public class RabbitMQConfig {
     private String exchangeName;
 
     @Bean
-    public TopicExchange notificacionesExchange() {
-        return new TopicExchange(exchangeName);
+    public DirectExchange notificacionesExchange() {
+        return new DirectExchange(exchangeName);
     }
 
     @Bean
