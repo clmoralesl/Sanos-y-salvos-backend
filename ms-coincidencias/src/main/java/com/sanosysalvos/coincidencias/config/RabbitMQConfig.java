14cc0896dafdeb16302a119a7b74a80b385ca76c
clmoralesl <cl.moralesl@duocuc.cl>
2026-07-13 03:19:20 -0400

    Corrige serialización JSON en ms-coincidencias añadiendo Jackson2JsonMessageConverter al RabbitTemplate

--- ms-coincidencias/src/main/java/com/sanosysalvos/coincidencias/config/RabbitMQConfig.java
+++ ms-coincidencias/src/main/java/com/sanosysalvos/coincidencias/config/RabbitMQConfig.java
@@ -1,6 +1,9 @@
 package com.sanosysalvos.coincidencias.config;
 
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,5 +17,17 @@ public class RabbitMQConfig {
     @Bean
     public TopicExchange notificacionesExchange() {
         return new TopicExchange(exchangeName);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jackson2JsonMessageConverter());
+        return rabbitTemplate;
     }
 }
